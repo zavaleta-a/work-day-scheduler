4acd36e9e10b38c29169c1532f4b30d288803ccf
Alfredo Zavaleta <zavaleta@Alfredos-MacBook-Pro.local>
2021-08-23 02:34:29 -0500

    added psuedocode to script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// When the scheduler is open, the current day is at the top
+// When user scrolls, they can see timeblocks for business hours
+// Color code timeblocks for past, present, or future
+// When you click on a timeblock, you can enter an event
+// Create a save button that will save entered info into local storage
+// When the page is refreshed, the info entered is still shown
