b9577e7ca72d8adcadca4c6657348f8c26e79cd8
Alfredo Zavaleta <azavaleta07@gmail.com>
2021-08-28 20:07:36 -0500

    created function to track the hours throughout the day

--- script.js
+++ script.js
@@ -5,38 +5,54 @@
 
 // When the page is refreshed, the info entered is still shown
 
+// Variables for if else if statement
+
+var currentTime = moment().hour();
+
 // Use moment.js to show the date
 var date = moment().format('MMMM Do YYYY');
 // Line 29 is where the date goes.  #currentDay .lead
 $('#currentDay').html(date);
 
+
+function hourTracker() {
+    $('.time-block').each(function() {
+        var timeBlock = parseInt($(this).attr('id').split('hour')[1]);
+    
+})
 // Color code timeblocks (.past, .present, .future)
 // Change colors for past present and future in CSS
-if (timeBlock < currentTime) {
-    $(this).addClass('past');
-    $(this).removeClass('present');
-    $(this).removeClass('future');
-}
+    if (timeBlock < currentTime) {
+        $(this).addClass('past');
+        $(this).removeClass('present');
+        $(this).removeClass('future');
+    }
 
-else if (timeBlock === currentTime) {
-    $(this).removeClass('past');
-    $(this).addClass('present');
-    $(this).removeClass('future');
-}
+    else if (timeBlock === currentTime) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+        $(this).removeClass('future');
+    }
 
-else if (timeBlock > currentTime) {
-    $(this).removeClass('past');
-    $(this).removeClass('present');
-    $(this).addClass('future');
+    else if (timeBlock > currentTime) {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+    }
+
 }
 
 // Create a save button that will save entered info into local storage
 // Need to create an eventListener for .saveBtn
 $('.saveBtn').click(function() {
     console.log($(this).prev().val());
+    var text = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(time, text);
+    console.log($(this).prev().val());
 });
 
-localStorage.setItem(time, text);
+// Need to be able to get items that have been saved to local storage
+$('0900 .description').val(localStorage.getItem('0900'));
 
-var timeBlock
-var currentTime
